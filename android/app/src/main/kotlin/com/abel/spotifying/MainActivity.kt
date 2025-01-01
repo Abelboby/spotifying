906b8af5d3705c617668d1b2a075c38d7a1edd48
Abelboby <abeldavisboby@gmail.com>
2025-01-01 14:18:36 +0530

    Initial commit: Flutter Spotify project setup

---
+++ android/app/src/main/kotlin/com/abel/spotifying/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abel.spotifying
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/spotifying/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spotifying
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
